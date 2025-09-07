3707185a4bc07c67374c35daef2501427894a913
gabriela.gajerez@gmail.com <gab-jrz>
2025-09-07 17:06:18 -0300

    Actualización

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import morgan from 'morgan';
+import morgan from 'morgan'; // Logger
 import process from 'node:process';
 import helmet from 'helmet';
 import compression from 'compression';
